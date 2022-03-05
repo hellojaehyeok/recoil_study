25cf1cf71eef954b46babe46c78192776efef1a8
Song jae hyeok <wakij6587@naver.com>
2022-03-05 23:57:30 +0900

    Feat: react init

---
+++ recoil/src/pages/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = ({}) => {
+    
+
+    return(
+        <Container>
+        </Container>
+    )
+};
+
+export default Main;
+
+
+const Container = styled.div`
+`
